3283c7c5058773ca9df9d0bd2bd65fa7e4c1f4ed
lucianalfred <lucianalfred60@gmail.com>
2025-09-06 00:35:08 +0100

    feat: simple api

--- api/taskmanager/taskmanager/urls.py
+++ api/taskmanager/taskmanager/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from tasks.views import TaskViewSet
 
-router = router.DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
